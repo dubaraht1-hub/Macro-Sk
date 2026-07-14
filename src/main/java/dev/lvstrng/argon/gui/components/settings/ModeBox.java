6ba3a354da8d5df5d5e000ad72d17d08d342f0ae
dubaraht1-hub <dubaraht1@gmail.com>
2026-07-14 17:02:22 +0500

    Update ModeBox.java

--- src/main/java/dev/lvstrng/argon/gui/components/settings/ModeBox.java
+++ src/main/java/dev/lvstrng/argon/gui/components/settings/ModeBox.java
@@ -1,64 +1,126 @@
 package dev.lvstrng.argon.gui.components.settings;
 
 import dev.lvstrng.argon.gui.components.ModuleButton;
-import dev.lvstrng.argon.module.setting.ModeSetting;
 import dev.lvstrng.argon.module.setting.Setting;
 import dev.lvstrng.argon.utils.ColorUtils;
+import dev.lvstrng.argon.utils.RenderUtils;
 import dev.lvstrng.argon.utils.TextRenderer;
-import org.lwjgl.glfw.GLFW;
-
 import java.awt.*;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 
-public final class ModeBox extends RenderableSetting {
-	public final ModeSetting<?> setting;
-	private Color currentAlpha;
+public abstract class RenderableSetting {
+	public MinecraftClient mc = MinecraftClient.getInstance();
+	public ModuleButton parent;
+	public Setting<?> setting;
+	public int offset;
+	public Color currentColor;
+	public boolean mouseOver;
+	int x;
+	int y;
+	int width;
+	int height;
 
-	public ModeBox(ModuleButton parent, Setting<?> setting, int offset) {
-		super(parent, setting, offset);
-		this.setting = (ModeSetting<?>) setting;
+	public RenderableSetting(ModuleButton parent, Setting<?> setting, int offset) {
+		this.parent = parent;
+		this.setting = setting;
+		this.offset = offset;
+
+		this.x = parentX();
+		this.y = parentY() + parentOffset() + offset;
+		this.width = parentX() + parentWidth();
+		this.height = parentY() + parentOffset() + offset + parentHeight();
 	}
 
-	@Override
+	public int parentX() {
+		return parent.parent.getX();
+	}
+
+	public int parentY() {
+		return parent.parent.getY();
+	}
+
+	public int parentWidth() {
+		return parent.parent.getWidth();
+	}
+
+	public int parentHeight() {
+		return parent.parent.getHeight();
+	}
+
+	public int parentOffset() {
+		return parent.offset;
+	}
+
 	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-		super.render(context, mouseX, mouseY, delta);
+		updateMouseOver(mouseX, mouseY);
+		this.x = parentX();
+		this.y = parentY() + parentOffset() + offset;
+		this.width = parentX() + parentWidth();
+		this.height = parentY() + parentOffset() + offset + parentHeight();
 
-		int nameOffset = parentX() + 6;
-		TextRenderer.drawString(setting.getName() + ": ", context, nameOffset, (parentY() + parentOffset() + offset) + 9, new Color(245, 245, 245, 255).getRGB());
-		nameOffset += TextRenderer.getWidth(setting.getName() + ": ");
+		context.fill(x, y, width, height, currentColor.getRGB());
+	}
 
-		int modeOffset = nameOffset;
+	private void updateMouseOver(double mouseX, double mouseY) {
+		this.mouseOver = isHovered(mouseX, mouseY);
+	}
 
-		TextRenderer.drawString(setting.getMode().name(), context, modeOffset, (parentY() + parentOffset() + offset) + 9, new Color(245, 245, 245, 255).getRGB());
+	public void renderDescription(DrawContext context, int mouseX, int mouseY, float delta) {
+		if (isHovered(mouseX, mouseY) && setting.getDescription() != null && !parent.parent.dragging) {
+			CharSequence chars = setting.getDescription();
 
-		if (!parent.parent.dragging) {
-			int toHoverAlpha = isHovered(mouseX, mouseY) ? 15 : 0;
+			int tw = TextRenderer.getWidth(chars);
 
-			if (currentAlpha == null)
-				currentAlpha = new Color(255, 255, 255, toHoverAlpha);
-			else currentAlpha = new Color(255, 255, 255, currentAlpha.getAlpha());
+			int parentCenter = mc.getWindow().getScaledWidth() / 2;
+			int textCenter = parentCenter - tw / 2;
 
-			if (currentAlpha.getAlpha() != toHoverAlpha)
-				currentAlpha = ColorUtils.smoothAlphaTransition(0.05F, toHoverAlpha, currentAlpha);
+			RenderUtils.renderRoundedQuad(
+					context.getMatrices(),
+					new Color(100, 100, 100, 100),
+					textCenter - 5,
+					(mc.getWindow().getScaledHeight() / 2) + 294,
+					textCenter + tw + 5,
+					(mc.getWindow().getScaledHeight() / 2) + 318,
+					3,
+					10
+			);
 
-			context.fill(parentX(), parentY() + parentOffset() + offset, parentX() + parentWidth(), parentY() + parentOffset() + offset + parentHeight(), currentAlpha.getRGB());
+			TextRenderer.drawString(chars, context, textCenter, (mc.getWindow().getScaledHeight() / 2) + 300, Color.WHITE.getRGB());
 		}
 	}
 
-	@Override
+	public void onGuiClose() {
+		this.currentColor = null;
+	}
+
 	public void keyPressed(int keyCode, int scanCode, int modifiers) {
-		if(mouseOver && parent.extended) {
-			if(keyCode == GLFW.GLFW_KEY_BACKSPACE)
-				setting.setModeIndex(setting.getOriginalValue());
-		}
-		super.keyPressed(keyCode, scanCode, modifiers);
 	}
 
-	@Override
-	public void mouseClicked(double mouseX, double mouseY, int button) {
-		if (isHovered(mouseX, mouseY) && button == GLFW.GLFW_MOUSE_BUTTON_LEFT)
-			setting.cycle();
+	public boolean isHovered(double mouseX, double mouseY) {
+		return mouseX > parentX()
+				&& mouseX < parentX() + parentWidth()
+				&& mouseY > offset + parentOffset() + parentY()
+				&& mouseY < offset + parentOffset() + parentY() + parentHeight();
+	}
 
-		super.mouseClicked(mouseX, mouseY, button);
+	public void onUpdate() {
+		if (currentColor == null)
+			currentColor = new Color(0, 0, 0, 0);
+		else currentColor = new Color(0, 0, 0, currentColor.getAlpha());
+
+		int toAlpha = 120;
+
+		if (currentColor.getAlpha() != toAlpha)
+			currentColor = ColorUtils.smoothAlphaTransition(0.05F, toAlpha, currentColor);
+	}
+
+	public void mouseClicked(double mouseX, double mouseY, int button) {
+	}
+
+	public void mouseReleased(double mouseX, double mouseY, int button) {
+	}
+
+	public void mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
 	}
 }
