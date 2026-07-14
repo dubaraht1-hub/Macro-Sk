3d36eecb06747a9d666d488554018d5e2e449b79
dubaraht1-hub <dubaraht1@gmail.com>
2026-07-14 16:18:17 +0500

    Update ClickGui.java

--- src/main/java/dev/lvstrng/argon/gui/ClickGui.java
+++ src/main/java/dev/lvstrng/argon/gui/ClickGui.java
@@ -1,92 +1,148 @@
-package dev.lvstrng.argon.module.modules.client;
+package dev.lvstrng.argon.gui;
 
 import dev.lvstrng.argon.Argon;
-import dev.lvstrng.argon.event.events.PacketReceiveListener;
-import dev.lvstrng.argon.gui.ClickGui;
 import dev.lvstrng.argon.module.Category;
-import dev.lvstrng.argon.module.Module;
-import dev.lvstrng.argon.module.setting.BooleanSetting;
-import dev.lvstrng.argon.module.setting.MinMaxSetting;
-import dev.lvstrng.argon.module.setting.ModeSetting;
-import dev.lvstrng.argon.module.setting.NumberSetting;
-import dev.lvstrng.argon.utils.EncryptedString;
-import net.minecraft.class_3944;
-import net.minecraft.class_490;
-import org.lwjgl.glfw.GLFW;
+import dev.lvstrng.argon.module.modules.client.ClickGUI;
+import dev.lvstrng.argon.utils.ColorUtils;
+import dev.lvstrng.argon.utils.RenderUtils;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import net.minecraft.class_2561;
+import net.minecraft.class_310;
+import net.minecraft.class_332;
+import net.minecraft.class_437;
 
-public final class ClickGUI extends Module implements PacketReceiveListener {
-	public static final NumberSetting red = new NumberSetting(EncryptedString.of("Red"), 0, 255, 120, 1);
-	public static final NumberSetting green = new NumberSetting(EncryptedString.of("Green"), 0, 255, 90, 1);
-	public static final NumberSetting blue = new NumberSetting(EncryptedString.of("Blue"), 0, 255, 255, 1);
+import static dev.lvstrng.argon.Argon.mc;
 
-	public static final NumberSetting alphaWindow = new NumberSetting(EncryptedString.of("Window Alpha"), 0, 255, 180, 1);
+public final class ClickGui extends class_437 {
+	public List<Window> windows = new ArrayList<>();
+	public Color currentColor;
+	private static final StackWalker sw = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 
-	public static final BooleanSetting breathing = new BooleanSetting(EncryptedString.of("Breathing"), true)
-			.setDescription(EncryptedString.of("Smooth breathing pulse effect (works when Rainbow is disabled)"));
-	public static final BooleanSetting rainbow = new BooleanSetting(EncryptedString.of("Rainbow"), false)
-			.setDescription(EncryptedString.of("Dynamic rainbow gradient shifting across elements"));
+	public ClickGui() {
+		super(class_2561.method_43473());
 
-	public static final BooleanSetting background = new BooleanSetting(EncryptedString.of("Background"), true)
-			.setDescription(EncryptedString.of("Renders the sleek backdrop blur for the Click GUI"));
-	public static final BooleanSetting customFont = new BooleanSetting(EncryptedString.of("Custom Font"), true);
-
-	private final BooleanSetting preventClose = new BooleanSetting(EncryptedString.of("Prevent Close"), true)
-			.setDescription(EncryptedString.of("Stops server freeze plugins from closing your GUI screen"));
-
-	public static final NumberSetting roundQuads = new NumberSetting(EncryptedString.of("Roundness"), 1, 10, 4, 1);
-	public static final ModeSetting<AnimationMode> animationMode = new ModeSetting<>(EncryptedString.of("Animations"), AnimationMode.Normal, AnimationMode.class);
-	public static final BooleanSetting antiAliasing = new BooleanSetting(EncryptedString.of("MSAA"), true)
-			.setDescription(EncryptedString.of("Smoothens rendering and GUI curves | Minimal performance impact |"));
-
-	public enum AnimationMode {
-		Normal, Positive, Off;
-	}
-
-	public ClickGUI() {
-		super(EncryptedString.of("SyWare"),
-				EncryptedString.of("SyWare Official By JENNY "),
-				GLFW.GLFW_KEY_RIGHT_SHIFT,
-				Category.CLIENT);
-
-		addSettings(red, green, blue, alphaWindow, breathing, rainbow, background, preventClose, roundQuads, animationMode, antiAliasing);
-	}
-
-	@Override
-	public void onEnable() {
-		eventManager.add(PacketReceiveListener.class, this);
-		Argon.INSTANCE.previousScreen = mc.field_1755;
-
-		if (Argon.INSTANCE.clickGui != null) {
-			mc.method_29970(Argon.INSTANCE.clickGui);
-		} else if (mc.field_1755 instanceof class_490) {
-			Argon.INSTANCE.guiInitialized = true;
+		int offsetX = 40;
+		for (Category category : Category.values()) {
+			windows.add(new Window(offsetX, 50, 240, 28, category, this));
+			offsetX += 260;
 		}
+	}
 
-		super.onEnable();
+	public boolean isDraggingAlready() {
+		for(Window window : windows)
+			if(window.dragging)
+				return true;
+
+		return false;
 	}
 
 	@Override
-	public void onDisable() {
-		eventManager.remove(PacketReceiveListener.class, this);
-
-		if (mc.field_1755 instanceof ClickGui) {
-			Argon.INSTANCE.clickGui.method_25419();
-			mc.method_29970(Argon.INSTANCE.previousScreen);
-			Argon.INSTANCE.clickGui.onGuiClose();
-		} else if (mc.field_1755 instanceof class_490) {
-			Argon.INSTANCE.guiInitialized = false;
+	protected void method_56131() {
+		if (field_22787 == null) {
+			return;
 		}
-
-		super.onDisable();
+		super.method_56131();
 	}
 
 	@Override
-	public void onPacketReceive(PacketReceiveEvent event) {
-		if (Argon.INSTANCE.guiInitialized) {
-			if (event.packet instanceof class_3944) {
-				if (preventClose.getValue())
-					event.cancel();
+	public void method_25394(class_332 context, int mouseX, int mouseY, float delta) {
+		if (mc.field_1755 == this) {
+			if (Argon.INSTANCE.previousScreen != null)
+				Argon.INSTANCE.previousScreen.method_25394(context, 0, 0, delta);
+
+			if (currentColor == null)
+				currentColor = new Color(10, 10, 15, 0);
+			else currentColor = new Color(10, 10, 15, currentColor.getAlpha());
+
+			if (currentColor.getAlpha() != (ClickGUI.background.getValue() ? 160 : 0))
+				currentColor = ColorUtils.smoothAlphaTransition(0.06F, ClickGUI.background.getValue() ? 160 : 0, currentColor);
+
+			if (mc.field_1755 instanceof ClickGui)
+				context.method_25294(0, 0, mc.method_22683().method_4480(), mc.method_22683().method_4507(), currentColor.getRGB());
+
+			RenderUtils.unscaledProjection();
+			mouseX *= (int) class_310.method_1551().method_22683().method_4495();
+			mouseY *= (int) class_310.method_1551().method_22683().method_4495();
+			super.method_25394(context, mouseX, mouseY, delta);
+
+			for (Window window : windows) {
+				window.render(context, mouseX, mouseY, delta);
+				window.updatePosition(mouseX, mouseY, delta);
 			}
+
+			RenderUtils.scaledProjection();
 		}
+	}
+
+	@Override
+	public boolean method_25404(int keyCode, int scanCode, int modifiers) {
+		for (Window window : windows)
+			window.keyPressed(keyCode, scanCode, modifiers);
+
+		return super.method_25404(keyCode, scanCode, modifiers);
+	}
+
+	@Override
+	public boolean method_25402(double mouseX, double mouseY, int button) {
+		mouseX *= (int) class_310.method_1551().method_22683().method_4495();
+		mouseY *= (int) class_310.method_1551().method_22683().method_4495();
+
+		for (Window window : windows)
+			window.mouseClicked(mouseX, mouseY, button);
+
+		return super.method_25402(mouseX, mouseY, button);
+	}
+
+	@Override
+	public boolean method_25403(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
+		mouseX *= (int) class_310.method_1551().method_22683().method_4495();
+		mouseY *= (int) class_310.method_1551().method_22683().method_4495();
+		for (Window window : windows)
+			window.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
+
+		return super.method_25403(mouseX, mouseY, button, deltaX, deltaY);
+	}
+
+	@Override
+	public boolean method_25401(double mouseX, double mouseY, double horizontalAmount, double verticalAmount) {
+		class_310 mc = class_310.method_1551();
+		mouseY *= mc.method_22683().method_4495();
+
+		for (Window window : windows)
+			window.mouseScrolled(mouseX, mouseY, horizontalAmount, verticalAmount);
+
+		return super.method_25401(mouseX, mouseY, horizontalAmount, verticalAmount);
+	}
+
+	@Override
+	public boolean method_25421() {
+		return false;
+	}
+
+	@Override
+	public void method_25419() {
+		Argon.INSTANCE.getModuleManager().getModule(ClickGUI.class).setEnabledStatus(false);
+		onGuiClose();
+	}
+
+	public void onGuiClose() {
+		mc.method_29970(Argon.INSTANCE.previousScreen);
+		currentColor = null;
+
+		for (Window window : windows)
+			window.onGuiClose();
+	}
+
+	@Override
+	public boolean method_25406(double mouseX, double mouseY, int button) {
+		mouseX *= (int) class_310.method_1551().method_22683().method_4495();
+		mouseY *= (int) class_310.method_1551().method_22683().method_4495();
+
+		for (Window window : windows)
+			window.mouseReleased(mouseX, mouseY, button);
+
+		return super.method_25406(mouseX, mouseY, button);
 	}
 }
