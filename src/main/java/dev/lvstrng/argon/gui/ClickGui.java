35f64c7c1d98e2b6c835df3dddf18a80e8642be3
dubaraht1-hub <dubaraht1@gmail.com>
2026-07-14 16:17:18 +0500

    Update ClickGui.java

--- src/main/java/dev/lvstrng/argon/gui/ClickGui.java
+++ src/main/java/dev/lvstrng/argon/gui/ClickGui.java
@@ -5,29 +5,28 @@ import dev.lvstrng.argon.module.Category;
 import dev.lvstrng.argon.module.modules.client.ClickGUI;
 import dev.lvstrng.argon.utils.ColorUtils;
 import dev.lvstrng.argon.utils.RenderUtils;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
-
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import net.minecraft.class_2561;
+import net.minecraft.class_310;
+import net.minecraft.class_332;
+import net.minecraft.class_437;
 
 import static dev.lvstrng.argon.Argon.mc;
 
-public final class ClickGui extends Screen {
+public final class ClickGui extends class_437 {
 	public List<Window> windows = new ArrayList<>();
 	public Color currentColor;
 	private static final StackWalker sw = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 
 	public ClickGui() {
-		super(Text.empty());
+		super(class_2561.method_43473());
 
-		int offsetX = 50;
+		int offsetX = 40;
 		for (Category category : Category.values()) {
-			windows.add(new Window(offsetX, 50, 230, 30, category, this));
-			offsetX += 250;
+			windows.add(new Window(offsetX, 50, 240, 28, category, this));
+			offsetX += 260;
 		}
 	}
 
@@ -40,33 +39,33 @@ public final class ClickGui extends Screen {
 	}
 
 	@Override
-	protected void setInitialFocus() {
-		if (client == null) {
+	protected void method_56131() {
+		if (field_22787 == null) {
 			return;
 		}
-		super.setInitialFocus();
+		super.method_56131();
 	}
 
 	@Override
-	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-		if (mc.currentScreen == this) {
+	public void method_25394(class_332 context, int mouseX, int mouseY, float delta) {
+		if (mc.field_1755 == this) {
 			if (Argon.INSTANCE.previousScreen != null)
-				Argon.INSTANCE.previousScreen.render(context, 0, 0, delta);
+				Argon.INSTANCE.previousScreen.method_25394(context, 0, 0, delta);
 
 			if (currentColor == null)
-				currentColor = new Color(0, 0, 0, 0);
-			else currentColor = new Color(0, 0, 0, currentColor.getAlpha());
+				currentColor = new Color(10, 10, 15, 0);
+			else currentColor = new Color(10, 10, 15, currentColor.getAlpha());
 
-			if (currentColor.getAlpha() != (ClickGUI.background.getValue() ? 200 : 0))
-				currentColor = ColorUtils.smoothAlphaTransition(0.05F, ClickGUI.background.getValue() ? 200 : 0, currentColor);
+			if (currentColor.getAlpha() != (ClickGUI.background.getValue() ? 160 : 0))
+				currentColor = ColorUtils.smoothAlphaTransition(0.06F, ClickGUI.background.getValue() ? 160 : 0, currentColor);
 
-			if (mc.currentScreen instanceof ClickGui)
-				context.fill(0, 0, mc.getWindow().getWidth(), mc.getWindow().getHeight(), currentColor.getRGB());
+			if (mc.field_1755 instanceof ClickGui)
+				context.method_25294(0, 0, mc.method_22683().method_4480(), mc.method_22683().method_4507(), currentColor.getRGB());
 
 			RenderUtils.unscaledProjection();
-			mouseX *= (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-			mouseY *= (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-			super.render(context, mouseX, mouseY, delta);
+			mouseX *= (int) class_310.method_1551().method_22683().method_4495();
+			mouseY *= (int) class_310.method_1551().method_22683().method_4495();
+			super.method_25394(context, mouseX, mouseY, delta);
 
 			for (Window window : windows) {
 				window.render(context, mouseX, mouseY, delta);
@@ -78,58 +77,58 @@ public final class ClickGui extends Screen {
 	}
 
 	@Override
-	public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+	public boolean method_25404(int keyCode, int scanCode, int modifiers) {
 		for (Window window : windows)
 			window.keyPressed(keyCode, scanCode, modifiers);
 
-		return super.keyPressed(keyCode, scanCode, modifiers);
+		return super.method_25404(keyCode, scanCode, modifiers);
 	}
 
 	@Override
-	public boolean mouseClicked(double mouseX, double mouseY, int button) {
-		mouseX *= (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-		mouseY *= (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+	public boolean method_25402(double mouseX, double mouseY, int button) {
+		mouseX *= (int) class_310.method_1551().method_22683().method_4495();
+		mouseY *= (int) class_310.method_1551().method_22683().method_4495();
 
 		for (Window window : windows)
 			window.mouseClicked(mouseX, mouseY, button);
 
-		return super.mouseClicked(mouseX, mouseY, button);
+		return super.method_25402(mouseX, mouseY, button);
 	}
 
 	@Override
-	public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
-		mouseX *= (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-		mouseY *= (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+	public boolean method_25403(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
+		mouseX *= (int) class_310.method_1551().method_22683().method_4495();
+		mouseY *= (int) class_310.method_1551().method_22683().method_4495();
 		for (Window window : windows)
 			window.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
 
-		return super.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
+		return super.method_25403(mouseX, mouseY, button, deltaX, deltaY);
 	}
 
 	@Override
-	public boolean mouseScrolled(double mouseX, double mouseY, double horizontalAmount, double verticalAmount) {
-		MinecraftClient mc = MinecraftClient.getInstance();
-		mouseY *= mc.getWindow().getScaleFactor();
+	public boolean method_25401(double mouseX, double mouseY, double horizontalAmount, double verticalAmount) {
+		class_310 mc = class_310.method_1551();
+		mouseY *= mc.method_22683().method_4495();
 
 		for (Window window : windows)
 			window.mouseScrolled(mouseX, mouseY, horizontalAmount, verticalAmount);
 
-		return super.mouseScrolled(mouseX, mouseY, horizontalAmount, verticalAmount);
+		return super.method_25401(mouseX, mouseY, horizontalAmount, verticalAmount);
 	}
 
 	@Override
-	public boolean shouldPause() {
+	public boolean method_25421() {
 		return false;
 	}
 
 	@Override
-	public void close() {
+	public void method_25419() {
 		Argon.INSTANCE.getModuleManager().getModule(ClickGUI.class).setEnabledStatus(false);
 		onGuiClose();
 	}
 
 	public void onGuiClose() {
-		mc.setScreenAndRender(Argon.INSTANCE.previousScreen);
+		mc.method_29970(Argon.INSTANCE.previousScreen);
 		currentColor = null;
 
 		for (Window window : windows)
@@ -137,13 +136,13 @@ public final class ClickGui extends Screen {
 	}
 
 	@Override
-	public boolean mouseReleased(double mouseX, double mouseY, int button) {
-		mouseX *= (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-		mouseY *= (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+	public boolean method_25406(double mouseX, double mouseY, int button) {
+		mouseX *= (int) class_310.method_1551().method_22683().method_4495();
+		mouseY *= (int) class_310.method_1551().method_22683().method_4495();
 
 		for (Window window : windows)
 			window.mouseReleased(mouseX, mouseY, button);
 
-		return super.mouseReleased(mouseX, mouseY, button);
+		return super.method_25406(mouseX, mouseY, button);
 	}
 }
